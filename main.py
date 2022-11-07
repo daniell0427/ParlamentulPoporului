5a715d6591d36ec07b27ef17cd1e2aade48eb036
Daniel Ciocan <ciocandaniel45@gmail.com>
2022-11-07 21:11:43 +0200

    Am facut root + am organizat frontend cu un layout
    Adi e gay ca nu raspunde la telefon

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/lege1")
+def lege1():
+    return render_template("lege1.html")
+
+@app.route("/inregistrare")
+def inregistrare():
+    return render_template("form.html")
+
+@app.route("/adauga_proiect")
+def adauga_proiect():
+    return render_template("adaugaProiect.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
