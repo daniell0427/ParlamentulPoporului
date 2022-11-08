26027041e31bd444d8e05fb6a1c00a67269423d1
Daniel Ciocan <ciocandaniel45@gmail.com>
2022-11-08 09:02:17 +0200

    AdaugaProiect(nefunctional)

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+articole = [
+    {
+    'titlu': '',
+    'nume': '',
+    'data': '',
+    'text': ''
+    }
+]
+    
 @app.route("/")
 @app.route("/acasa")
 def acasa():
